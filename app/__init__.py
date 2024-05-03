92844fb4e6fe19f5f062b036c9e493b3de88a4ff
Владислав <salo3666@mail.ru>
2024-05-03 14:23:57 +0300

    flask project

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from config import Config
 from flask_pymongo import PyMongo
+from flask_session import Session
 
 mongo = PyMongo()
 
@@ -9,6 +10,30 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     mongo.init_app(app)
+
+    app.config['SESSION_TYPE'] = 'filesystem'
+    Session(app)
+
+    from app.main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
+
+from flask import Flask
+from config import Config
+from flask_pymongo import PyMongo
+from flask_session import Session
+
+mongo = PyMongo()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    mongo.init_app(app)
+
+    app.config['SESSION_TYPE'] = 'filesystem'
+    Session(app)
 
     from app.main import main as main_blueprint
     app.register_blueprint(main_blueprint)
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,26 +1,51 @@
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, session
 from . import main
 from app import mongo
 
 
+@main.route('/')
+@main.route('/index')
+def index():
+    return render_template('index.html')
+
+
 @main.route('/register', methods=['GET', 'POST'])
 def register():
+    if 'username' in session:
+        return redirect(url_for('main.home'))
+
     if request.method == 'POST':
         username = request.form.get('username')
         email = request.form.get('email')
         password = request.form.get('password')
 
         users = mongo.db.users
+        existing_user = users.find_one({'username': username})
+        if existing_user:
+            return 'Пользователь с таким именем уже зарегистрирован!'
+
         users.insert_one({'username': username, 'email': email, 'password': password})
+
+        session['username'] = username
 
         return redirect(url_for('main.home'))
 
     return render_template('register.html')
 
+
 @main.route('/home')
 def home():
-    return render_template('home.html')
+    if 'username' not in session:
+        return redirect(url_for('main.index'))
+
+    username = session['username']
+
+    return render_template('home.html', username=username)
+
 
 @main.route('/tasks')
 def tasks():
+    if 'username' not in session:
+        return redirect(url_for('main.index'))
+
     return render_template('tasks.html')
